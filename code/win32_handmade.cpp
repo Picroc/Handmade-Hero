fbf3ffe39bbbd049aa5ab9857c9fe7ab84fff8f0
Alex Logachev <kingofrootelement@gmail.com>
2025-08-14 23:33:07 +0100

    Day 1

---
+++ code/win32_handmade.cpp
@@ -0,0 +1,12 @@
+#include <windows.h>
+
+int CALLBACK WinMain(
+    HINSTANCE hInstance,
+    HINSTANCE hPrevInstance,
+    LPSTR lpCmdLine,
+    int nCmdShow
+) {
+    MessageBoxA(0, "This is handmade Hero", "Handmade Hero", MB_OK|MB_ICONINFORMATION);
+
+    return(0);
+}
---
+++ misc/shell.bat
@@ -0,0 +1,3 @@
+@echo off
+call "c:\Program Files\Microsoft Visual Studio\2022\Community\VC\Auxiliary\Build\vcvarsall" x64
+set path=h:\misc;%path%
